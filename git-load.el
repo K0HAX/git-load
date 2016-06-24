ed0144c8648e48992c7fa6e4441ae49e448b5e1b
Michael Englehorn <michael@englehorn.com>
2016-06-24 15:44:32 -0500

    Added header information.

--- git-load.el
+++ git-load.el
@@ -1,3 +1,21 @@
+;;; git-load.el --- Load git repositories as packages.
+;; Copyright 2016 Michael Englehorn
+;;
+;; Author: Michael Englehorn <michael@englehorn.com>
+;; Maintainer: Michael Englehorn <michael@englehorn.com>
+;; Keywords: git git-load
+;; URL: https://github.com/K0HAX/git-load
+;; Created: 24 June 2016
+;; Version: 0.1.1
+;; Package-Requires: ((git "0.0.0"))
+
+;;; Commentary:
+;;
+;; An API to load packages from arbitrary git repositories
+;;
+
+;;; Code:
+
 (require 'git)
 
 (defvar git-load-default-directory "~/.emacs.d/git/")
